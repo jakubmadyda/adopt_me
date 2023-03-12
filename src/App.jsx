4449485c3e834f00b62e6d9f82edcc4b0b0d226d
Jakub Madyda <jakub.madyda17@gmail.com>
2023-03-12 13:22:52 +0100

    create AdoptPetContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Details from "./Details";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import AdoptPetContext from "./AdoptPetContext";
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             staleTime: Infinity,
-            cacheTime: Infinity
-        }
-    }
+            cacheTime: Infinity,
+        },
+    },
 });
 
-
 const App = () => {
-  return (
-    <div>
-        <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-            <header>
-                <Link to="/">Adopt Me!</Link>
-            </header>
-            <Routes>
-                <Route path="/" element={<SearchParams />} />
-                <Route path="/details/:id" element={<Details />}/>
-            </Routes>
-        </QueryClientProvider>
-        </BrowserRouter>
-    </div>
-  );
+    const adoptPet = useState(null);
+    // const [theme, setTheme] = useState('darkMode')
+
+    return (
+        <div>
+            <BrowserRouter>
+                <QueryClientProvider client={queryClient}>
+                    <AdoptPetContext.Provider value={adoptPet}>
+                        <header>
+                            <Link to="/">Adopt Me!</Link>
+                        </header>
+                        <Routes>
+                            <Route path="/" element={<SearchParams />} />
+                            <Route path="/details/:id" element={<Details />} />
+                        </Routes>
+                    </AdoptPetContext.Provider>
+                </QueryClientProvider>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 const container = document.getElementById("root");
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import {useContext, useState} from "react";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
 import {useQuery} from "@tanstack/react-query";
 import fetchSearch from "./fetchSearch";
+import adoptPetContext from "./AdoptPetContext";
+import AdoptPetContext from "./AdoptPetContext";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -15,6 +17,7 @@ const SearchParams = () => {
     const [animal, setAnimal] = useState("");
     const [breeds] = useBreedList(animal);
     const results = useQuery(['search', requestParams], fetchSearch);
+    const [adoptedPet] = useContext(AdoptPetContext);
 
     const pets = results?.data?.pets ?? [];
 
@@ -25,13 +28,18 @@ const SearchParams = () => {
                     event.preventDefault();
                     const formData = new FormData(event.target);
                     const data = {
-                        animal: formData.get('animal') ?? '',
-                        breed: formData.get('breed') ?? '',
-                        location: formData.get('location') ?? ''
-                    }
+                        animal: formData.get("animal") ?? "",
+                        breed: formData.get("breed") ?? "",
+                        location: formData.get("location") ?? "",
+                    };
                     setRequestParams(data);
                 }}
             >
+                {adoptedPet ? (
+                    <div className="pet image-container">
+                        <img src={adoptedPet?.images[0]} alt={adoptedPet?.name} />
+                    </div>
+                ) : null}
                 <label htmlFor="location">Location</label>
                 <input
                     name="location"
@@ -56,11 +64,7 @@ const SearchParams = () => {
                 </select>
 
                 <label htmlFor="breed">Breed</label>
-                <select
-                    name="breed"
-                    id="breed"
-                    disabled={!breeds.length}
-                >
+                <select name="breed" id="breed" disabled={!breeds.length}>
                     {breeds.map((breed) => (
                         <option value={breed} key={breed}>
                             {breed}
