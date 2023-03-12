4a372665375539bfb389aba0b8ca15c1bef3303f
Jakub Madyda <jakub.madyda17@gmail.com>
2023-03-12 12:00:35 +0100

    create error boundary class

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,17 +1,19 @@
 import { Component } from "react";
 
 class Carousel extends Component {
-    constructor(props) {
-        super(props);
-        this.props = props;
-        this.state = { active: 0 };
+    state = {
+        active: 0,
+    };
+
+    static defaultProps = {
+        images: ["http://pets-images.dev-apis.com/pets/none.jpg"]
     }
 
     handleIndexClick = (event) => {
         this.setState({
-            active: +event.target.dataset.index
-        })
-    }
+            active: +event.target.dataset.index,
+        });
+    };
 
     render() {
         const { images } = this.props;
@@ -21,6 +23,8 @@ class Carousel extends Component {
                 <img src={images[active]} alt="animal" />
                 <div className="carousel-smaller">
                     {images.map((image, idx) => (
+
+                        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
                         <img
                             src={image}
                             alt="animal thumbnail"
