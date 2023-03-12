24f72942602994c68f721e9fded90123d64b6567
Jakub Madyda <jakub.madyda17@gmail.com>
2023-03-12 12:46:46 +0100

    create portal functionality in modal component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,35 +1,52 @@
 import { useParams } from "react-router-dom";
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
+import { useState } from "react";
 
 const Details = () => {
     const { id } = useParams();
-    const results = useQuery(['details', id], fetchPet);
+    const results = useQuery(["details", id], fetchPet);
+    const [showModal, setShowModal] = useState(false);
 
-    if(results.isLoading){
+    if (results.isLoading) {
         return (
-
             <div className="loading-pane">
-                <h2 className="loader">
-                    🌀
-                </h2>
+                <h2 className="loader">🌀</h2>
             </div>
-
-        )
+        );
     }
 
     const pet = results.data.pets[0];
 
     return (
         <div className="details">
-            <Carousel images={pet.images}/>
+            <Carousel images={pet.images} />
             <div>
                 <h1>{pet.name}</h1>
                 <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
-                <button>Adopt {pet.name}</button>
+                <button
+                    onClick={() => {
+                        setShowModal(true);
+                    }}
+                >
+                    Adopt {pet.name}
+                </button>
                 <p>{pet.description}</p>
+
+                {showModal && (
+                    <Modal>
+                        <div>
+                            <h1>Would you like to adopt {pet.name}?</h1>
+                            <div className="buttons">
+                                <button>Yes!</button>
+                                <button onClick={() => {setShowModal(false)}}>No</button>
+                            </div>
+                        </div>
+                    </Modal>
+                )}
             </div>
         </div>
     );
@@ -37,10 +54,10 @@ const Details = () => {
 
 function DetailsErrorBoundary(props) {
     return (
-        <ErrorBoundary >
-            <Details {...props}/>
+        <ErrorBoundary>
+            <Details {...props} />
         </ErrorBoundary>
-    )
+    );
 }
 
 export default DetailsErrorBoundary;
